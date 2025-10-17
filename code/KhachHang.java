0bd2e2300a0d052a02e20863dab3a48880ed6de0
MaGiaVy <3124410414@sv.sgu.edu.vn>
2025-10-17 07:45:40 +0700

    up file

---
+++ code/KhachHang.java
@@ -0,0 +1,30 @@
+public class KhachHang extends ConNguoi {
+    public String MaKH;
+    public static int dem = 0;
+
+    public KhachHang() {
+        MaKH = "";
+    }
+
+    public KhachHang(String Hoten, String Diachi, long Sdt, String MaKH) {
+        super(Hoten, Diachi, Sdt);
+        this.MaKH = String.format("KH%03d", dem);
+    }
+
+    @Override
+    public void Nhap() {
+        super.Nhap();
+        dem++;
+        MaKH = String.format("KH%03d", dem);
+    }
+
+    @Override
+    public void Xuat() {
+        System.out.printf("| %-6s | %-21s | %-20s | %-15s |\n", MaKH, Hoten, Diachi, Sdt);
+    }
+
+    public String getMaKH() {
+        return MaKH;
+    }
+
+}
